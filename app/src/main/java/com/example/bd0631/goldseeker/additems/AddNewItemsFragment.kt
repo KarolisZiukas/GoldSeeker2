ec6666ce80d78caae5bba73d0215eb17e9f80cc0
Karolis <karolis.ziu@gmail.com>
2019-01-01 22:58:04 +0200

    add tensor flow

--- app/src/main/java/com/example/bd0631/goldseeker/additems/AddNewItemsFragment.kt
+++ app/src/main/java/com/example/bd0631/goldseeker/additems/AddNewItemsFragment.kt
@@ -1,8 +1,12 @@
 package com.example.bd0631.goldseeker.additems
 
 import android.databinding.DataBindingUtil
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,8 +14,23 @@ import com.example.bd0631.goldseeker.R
 import com.example.bd0631.goldseeker.databinding.FragmentAddNewItemsBinding
 import com.example.bd0631.goldseeker.utils.LocationHelper
 import kotlinx.android.synthetic.main.fragment_add_new_items.*
+import java.io.IOException
 
 class AddNewItemsFragment: Fragment(), View.OnClickListener {
+
+  private val TAG = "TfLiteCameraDemo"
+
+  private val HANDLE_THREAD_NAME = "CameraBackground"
+
+  private val lock = Any()
+  private var runClassifier = false
+  private var classifier: ImageClassifier? = null
+
+  /** An additional thread for running tasks that shouldn't block the UI.  */
+  private var backgroundThread: HandlerThread? = null
+
+  /** A [Handler] for running tasks in the background.  */
+  private var backgroundHandler: Handler? = null
 
   lateinit var databinding: FragmentAddNewItemsBinding
 
@@ -32,6 +51,12 @@ class AddNewItemsFragment: Fragment(), View.OnClickListener {
     viewModel = (activity as AddNewItemsActivity).obtainViewModel()
     databinding.viewModel = viewModel
     bt_add_items.setOnClickListener(this)
+    try {
+      classifier = ImageClassifier(activity as AddNewItemsActivity)
+    } catch (e: IOException) {
+      Log.e(TAG, "Failed to initialize an image classifier.")
+    }
+    startBackgroundThread()
   }
 
   override fun onClick(p0: View?) {
@@ -46,4 +71,71 @@ class AddNewItemsFragment: Fragment(), View.OnClickListener {
     }
   }
 
+  private fun showToast(text: String) {
+    val activity = activity
+    activity?.runOnUiThread { tv_tensor.text = text }
+  }
+
+  override fun onResume() {
+    super.onResume()
+    startBackgroundThread()
+  }
+
+  override fun onPause() {
+    stopBackgroundThread()
+    super.onPause()
+  }
+
+  override fun onDestroy() {
+    classifier?.close()
+    super.onDestroy()
+  }
+
+  private fun startBackgroundThread() {
+    backgroundThread = HandlerThread(HANDLE_THREAD_NAME)
+    backgroundThread?.start()
+    backgroundHandler = Handler(backgroundThread?.looper)
+    synchronized(lock) {
+      runClassifier = true
+    }
+    backgroundHandler?.post(periodicClassify)
+  }
+
+  /** Stops the background thread and its [Handler].  */
+  private fun stopBackgroundThread() {
+    backgroundThread?.quitSafely()
+    try {
+      backgroundThread?.join()
+//      backgroundThread = null
+//      backgroundHandler = null
+      synchronized(lock) {
+        runClassifier = false
+      }
+    } catch (e: InterruptedException) {
+      e.printStackTrace()
+    }
+
+  }
+
+  private val periodicClassify = object : Runnable {
+    override fun run() {
+      synchronized(lock) {
+        if (runClassifier) {
+          classifyFrame()
+        }
+      }
+      backgroundHandler?.post(this)
+    }
+  }
+
+  private fun classifyFrame() {
+    if (classifier == null || activity == null) {
+      showToast("Uninitialized Classifier or invalid context.")
+      return
+    }
+    val bitmap = BitmapFactory.decodeResource(resources, R.drawable.testmicro1)
+    val textToShow = classifier?.classifyFrame(bitmap)
+    bitmap.recycle()
+    showToast(textToShow!!)
+  }
 }
